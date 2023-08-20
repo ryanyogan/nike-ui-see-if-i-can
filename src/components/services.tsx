cdc2823afbb3268ab0be12de347fc30786d74871
Ryan Yogan <ryanyogan@gmail.com>
2023-08-20 18:04:00 -0500

    feat // services

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,3 +1,12 @@
+import { services } from "@/constants";
+import ServiceCard from "./service-card";
+
 export default function Services() {
-  return <div>Subscribe</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 }
