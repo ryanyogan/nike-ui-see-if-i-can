cdb645eef3960efa77b8a6f6c1e09ab87acc77af
Ryan Yogan <ryanyogan@gmail.com>
2023-08-20 22:34:45 -0500

    feat // footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,55 @@
+import { footerLinks, socialMedia } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { footerLogo } from "../../public/images";
+
 export default function Footer() {
-  return <div>Subscribe</div>;
+  return (
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <Link href="/">
+            <Image src={footerLogo} width={150} height={46} alt="footer logo" />
+          </Link>
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
+            Get shoes ready for the new term at your nearest nike store. Find
+            your perfect shoe you idiot.
+          </p>
+
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => (
+              <div
+                key={icon.alt}
+                className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
+              >
+                <Image src={icon.src} alt={icon.alt} height={24} width={24} />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+          {footerLinks.map((section) => (
+            <div key={section.title}>
+              <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">
+                {section.title}
+              </h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li
+                    className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray"
+                    key={link.name}
+                  >
+                    <Link href={link.link}>{link.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center"></div>
+    </footer>
+  );
 }
